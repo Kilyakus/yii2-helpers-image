a98583a07714ff81470d719132516f4fc3d1292d
Kilyakus <aleksandr.kilyakov@gmail.com>
2019-10-04 06:53:52 +0500

    initial commit

--- src/Avatar.php
+++ src/Avatar.php
@@ -63,8 +63,8 @@ class Avatar
 
         $info = pathinfo($filename);
         $thumbName = $info['filename'] . '-' . md5( filemtime($filename) . (int)$width . (int)$height . (int)$crop ) . '.' . $info['extension'];
-        $thumbFile = Yii::getAlias('@webroot') . DIRECTORY_SEPARATOR . Upload::$UPLOADS_DIR . DIRECTORY_SEPARATOR . 'avatars' . DIRECTORY_SEPARATOR . $thumbName;
-        $thumbWebFile = '/' . Upload::$UPLOADS_DIR . '/avatars/' . $thumbName;
+        $thumbFile = Yii::getAlias('@webroot') . DIRECTORY_SEPARATOR . Upload::$UPLOADS_DIR . DIRECTORY_SEPARATOR . 'thumbs' . DIRECTORY_SEPARATOR . $thumbName;
+        $thumbWebFile = '/' . Upload::$UPLOADS_DIR . '/thumbs/' . $thumbName;
         if(file_exists($thumbFile)){
             return $thumbWebFile;
         }
@@ -73,5 +73,74 @@ class Avatar
         }
 
         return '';
+    }
+
+    static function merge($files, $size = 300)
+    {
+        $width = $height = $size;
+        $resImage = imagecreatetruecolor($width, $height);
+
+        $count = count($files);
+
+        $filename = '';
+
+        for ($i = 0; $i < $count; $i++) {
+
+            if($i > 4){
+                break;
+            }
+
+            $info = pathinfo($files[$i]);
+            $image = Yii::getAlias('@webroot') . $files[$i];
+            $filename .= md5( $info['filename'] . '-' . filemtime($image) . (int)$width . (int)$height );
+
+            $image = imagecreatefromstring(file_get_contents($image));
+            $srcWidth = imagesx($image);
+            $srcHeight = imagesy($image);
+
+            if($count == 1){
+
+                $x = ($i == 1 || $i == 2 ? $width / 2 : 0);
+                $y = ($i > 1 ? $height / 2 : 0);
+                $h2 = intval(($i == 0) ? $height : $height / 2);
+                $w2 = intval(($i == 0) ? $width : $width / 2);
+
+            }elseif($count == 2){
+
+                $x = ($i == 1 ? ($width / 2) : 0);
+                $y = ($i > 1 ? $height / 2 : 0);
+                $h2 = intval($height);
+                $w2 = intval($width / 2);
+
+            }elseif($count == 3){
+
+                $x = ($i == 1 || $i == 2 ? $width / 2 : -($width / 7));
+                $y = ($i > 1 ? $height / 2 : 0);
+                $h2 = intval(($i == 0) ? $height : $height / 2);
+                $w2 = intval(($i == 0) ? $width / 1.3 : $width / 2);
+
+            }elseif($count > 3){
+
+                $x = ($i == 1 || $i == 2 ? $width / 2 : 0);
+                $y = ($i > 1 ? $height / 2 : 0);
+                $h2 = intval($height / 2);
+                $w2 = intval($width / 2);
+
+            }
+
+            imagecopyresampled($resImage, $image, $x, $y, 0, 0, $w2, $h2, $srcWidth, $srcHeight);
+        
+            imagedestroy($image);
+        }
+
+        $filename = 'group-' . md5($filename);
+
+        imagejpeg($resImage, Yii::getAlias('@webroot') . DIRECTORY_SEPARATOR . Upload::$UPLOADS_DIR . DIRECTORY_SEPARATOR . 'avatars' . DIRECTORY_SEPARATOR . $filename . '.png');
+
+        imagedestroy($resImage);
+
+        $thumbFile = '/' . Upload::$UPLOADS_DIR . '/avatars/' . $filename . '.png';
+
+        return $thumbFile;
     }
 }
